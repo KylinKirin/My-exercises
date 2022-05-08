65942a45fb7761633fe103cbe1b2ffaec1bbfebc
KylinKirin <89174806+KylinKirin@users.noreply.github.com>
2022-05-08 17:49:54 +0900

    Update Counter of Dates In a Specific Range.py

--- Counter of Dates In a Specific Range.py
+++ Counter of Dates In a Specific Range.py
@@ -8,6 +8,9 @@ def date_to_index(start, stop, *target):
     while True:
         dat = time.strftime('%Y%m%d', time.localtime((time.mktime(time.strptime(start, "%Y%m%d"))+i*one_day)))
         # print(dat)
+        if time.mktime(time.strptime(start, "%Y%m%d")) > time.mktime(time.strptime(stop, "%Y%m%d")):
+            print("Wrong range")
+            return
         date_list.append(str(dat))
         i += 1
         if str(dat) == stop:
@@ -21,8 +24,8 @@ def date_to_index(start, stop, *target):
             res.append(index + 1)
         except ValueError:
             print(str(date)+" Not found")
-    return "Respectively "+str(res)
+    print("Respectively "+str(res))
 
 
 if __name__ == '__main__':
-    print(date_to_index("19980101", "19981231", "19980328", "19980329", "19980929"))
+    date_to_index("19980101", "19981231", "19980101", "19980329", "19980929")
